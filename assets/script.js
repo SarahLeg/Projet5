9d8db16c80422ef580345e9c3791092d188888e4
Sarah <sarah.legrand06@laposte.net>
2024-05-10 23:36:21 +0200

    potentiel bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,67 @@
  const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-slides.addEventListener('click', () => {
-	
-})
+let index = 0;
+
+//j'importe les deux flèches
+const arrow_left = document.querySelector('.arrow_left');
+const arrow_right = document.querySelector('.arrow_right');
+
+//j'importe les éléments HTML qui vont être modifié à chaque évènement
+const tagLine = document.querySelector("#banner p");
+const bannerImg = document.querySelector(".banner-img");
+
+//je met les function click dans les eventListener (elles seront anonymes)
+arrow_left.addEventListener('click', () => { 
+	index--;
+	//console.log(index);
+	if(index < 0) {index=slides.length - 1};
+	tagLine.innerHTML = slides[index].tagLine;
+	bannerImg.src= slides[index].image;
+	//console.log(slides[index])
+	updateBulletPoints
+});
+
+arrow_right.addEventListener('click', () => {
+	index++;
+	//console.log(index);
+	if(index === 4) {index=0};
+	tagLine.innerHTML = slides[index].tagLine;
+	bannerImg.src= slides[index].image;
+	//console.log(slides[index])
+	updateBulletPoints
+});
+
+//je déclare l'élément dans lequel les dots vont se multiplier
+const bullet_points = document.querySelector('.dots');
+
+//je duplique les dots
+slides.forEach( () => {
+    const bullet = document.createElement('span');
+    bullet.classList.add('dot');
+    bullet_points.appendChild(bullet);
+});
+
+updateBulletPoints( () => {
+    bullets.forEach((bullet, i) => {
+        if (i === index) {
+            bullet.classList.add('_selected');
+        }
+    });
+});
